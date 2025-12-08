c87a6b34a8fce077ae7cdf79ed2a81ce65fb81a8
eshukla15 <eshukla15@gmail.com>
2025-12-08 10:06:51 +0530

    Completed

--- learningdjango/learningdjango/urls.py
+++ learningdjango/learningdjango/urls.py
@@ -15,10 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # when the user will type tweet
+    # the control will be passed to tweet app now
+    # and it will refer to urls.py inside tweet 
+    # to find out what to do furthur
+   
+    path('', include('tweet.urls')),
+    path('tweet/', include('tweet.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
